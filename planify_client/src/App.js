82f0130c7bc67f4d9278a20d48e7eac78081cb16
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2024-11-19 21:01:17 -0300

    Login creado

--- planify_client/src/App.js
+++ planify_client/src/App.js
@@ -5,6 +5,7 @@ import { UserProvider } from "./Context/UserContext";
 import { CardStatusProvider } from "./Context/CardStatusContext";
 import Login from "./Pages/Login/Login";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Register from "./Pages/Register/Register";
 
 
 const router = createBrowserRouter([
@@ -15,8 +16,14 @@ const router = createBrowserRouter([
   {
     path: "/Login",
     element: <Login/>
+  },
+  {
+    path: "/Register",
+    element: <Register/>
   }
 ]);
+
+
 
 function App() {
 
--- planify_client/src/Pages/Login/Login.jsx
+++ planify_client/src/Pages/Login/Login.jsx
@@ -1,29 +1,41 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../Componets/Navbar/Navbar'
 import Footer from '../../Componets/Footer/Footer'
 import "./Login.css"
 const Login = () => {
-  return (
-    <div className='container-body'>
-        <nav><Navbar/></nav>
-        <main className='d-flex  align-items-center justify-content-center'>
-            <div className='login-card col-6 text-center'>
-                
-                <h3 className='mt-3'>Welcome!!</h3>
+    
+    return (
+        <div className='container-body'>
+            <nav><Navbar /></nav>
+            <main className='d-flex  align-items-center justify-content-center'>
+                <div className='login-card col-6 text-center'>
 
-                <div className=''>
-                    <input type='text' placeholder='Enter your email' className='col-8 border rounded-2 m-2'></input>
-                    <input type='password' placeholder='Enter your password' className='col-8 border rounded-2 m-1'></input>
-            
-                    <p className='col-9 d-flex  align-items-center'>I forgot my password</p>
-            
+                    <h3 className='mt-3'>Welcome!!</h3>
+
+                    <div className="d-flex justify-content-center">
+                        <div className="col-8">
+                            <div>
+                                <input type='text' placeholder='Enter your email' className='col-12 border rounded-2 my-2'></input>
+                                <input type='password' placeholder='Enter your password' className='col-12 border rounded-2 '></input>
+                            </div>
+                            <div className='text-end'>
+                                <a className='Link-Login'>I forgot my password</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                    <button className='py-1 px-5 my-2 rounded-3 Button-Login'>Login</button>
+                    </div>
+                    <div className='my-2'>
+                    <a className=' text-decoration-none text-dark'>Don’t you have an account? Register here</a>
+                    </div>
                 </div>
-            </div>
-        </main>
-        <footer><Footer/></footer>
-    </div>
-  )
+
+            </main>
+            <footer><Footer /></footer>
+        </div>
+    )
 }
 
 export default Login
