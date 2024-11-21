55d41a5cc72482bd0a143c9272be002b785118c7
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2024-11-21 19:09:59 -0300

    Preparacion Popup

--- planify_client/src/Componets/Board/TaskCard/TaskCard.jsx
+++ planify_client/src/Componets/Board/TaskCard/TaskCard.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './TaskCard.css';
 
 const TaskCard = ({ description, priority, users }) => {
+    const [PopUp, SetPopUp] = useState(false)
     const renderStars = () => {
         return Array.from({ length: 5 }, (_, index) => (
             <span key={index} className={`star ${index < priority ? 'filled' : ''}`}>
@@ -23,13 +24,31 @@ const TaskCard = ({ description, priority, users }) => {
     };
 
     return (
-        <div className="task-card">
-            <p className="description">{description}</p>
-            <div className="task-details">
-                <div className="priority">{renderStars()}</div>
-                <div className="users">{renderUsers()}</div>
+        <>
+            <div className="task-card" onClick={() => (SetPopUp(!PopUp))}>
+                <p className="description">{description}</p>
+                <div className="task-details">
+                    <div className="priority">{renderStars()}</div>
+                    <div className="users">{renderUsers()}</div>
+                </div>
             </div>
-        </div>
+
+            {PopUp && (
+                <>
+                    <div id="fondo-gris" >
+                        <div id="cartel-rojo">
+
+
+                            Toy en el medio
+                            <button onClick={()=>(SetPopUp(false))}>Cerrar</button>
+
+
+                        </div>
+                    </div>
+
+    </>
+)}
+        </>
     );
 };
 
